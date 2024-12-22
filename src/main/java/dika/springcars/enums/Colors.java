b76d8fdd2db7b61923fa2000d94ed70cf8c84391
DinaraM <blueberrywitch@yandex.ru>
2024-12-22 21:58:30 +0300

    second commit

---
+++ src/main/java/dika/springcars/enums/Colors.java
@@ -0,0 +1,15 @@
+package dika.springcars.enums;
+
+public enum Colors {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    ORANGE,
+    PURPLE,
+    PINK,
+    BLACK,
+    WHITE,
+    GRAY,
+    BROWN
+}
